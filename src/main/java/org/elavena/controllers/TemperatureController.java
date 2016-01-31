37eb3252e9197595e3d00c3114a7f600b93b74d4
Hector Avena <amigoavena@gmail.com>
2016-01-31 12:47:17 +0900

    Initial Commit for BoilerPlate

---
+++ src/main/java/org/elavena/controllers/TemperatureController.java
@@ -0,0 +1,23 @@
+package org.elavena.controllers;
+
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TemperatureController {
+	
+    @RequestMapping("/")
+    public String index() {
+        return "Greetings from Spring Boot!";
+        
+    }
+    
+    @RequestMapping("/greeting")
+    public String greeting(@RequestParam(value="name", required=false, defaultValue="World") String name, Model model) {
+        model.addAttribute("name", name);
+        return "greeting";
+    }
+
+}
